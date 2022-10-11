5290bf5525d4331b1a364fbaa934d38612b19f30
Dohyun Kim <dolpin1402@gmail.com>
2022-10-11 20:55:09 +0900

    test code removal.

--- cmag/interface/main.py
+++
@@ -1,8 +0,0 @@
-from cmag.project import CMagProject
-
-project = CMagProject('workspace/kkk')
-# project.add_challenge('test challenge!')
-print(project.list_challenges())
-challenge = project.get_challenge_by_name('test challenge!')
-# challenge.create_file('jaja')
-print(challenge.list_files())
