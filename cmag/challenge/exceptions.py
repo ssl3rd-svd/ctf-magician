bbf0ddf9e7a9e2ea79f697300377c9cd6e3e84cb
ch4rli3kop <pch2180@gmail.com>
2022-10-18 05:18:10 +0900

    Update exceptions.py
    challenge

--- cmag/challenge/exceptions.py
+++ cmag/challenge/exceptions.py
@@ -1,31 +1,76 @@
-# base dummy class
-class ChallengeFailed(Exception):
-    pass
+# base of challenge module
+class ChallFailed(Exception):
+    def __str__(self) -> str:
+        return f'ChallFailed'
 
 # class of challenge_impl.py
-class CMagChallengeImplFailed(ChallengeFailed):
-    pass
+class CMagChallImplFailed(ChallFailed):
+    def __str__(self) -> str:
+        return f'CMagChallImplFailed'
 
 # class of challenge.py
-class CMagChallengeFailed(CMagChallengeImplFailed):
-    pass
+class CMagChallFailed(CMagChallImplFailed):
+    def __str__(self) -> str:
+        return f'CMagChallFailed'
+
+# class of manager_impl.py
+class CMagChallMgrImplFailed(ChallFailed):
+    def __str__(self) -> str:
+        return f'CMagChallMgrImplFailed'
 
 # class of manager.py
-class CMagChallengeMangerFailed(ChallengeFailed):
-    pass
+class CMagChallMgrFailed(CMagChallMgrImplFailed):
+    def __str__(self) -> str:
+        return f'CMagChallMgrFailed'
 
 # class of model.py
-class CMagChallengeModelFailed(ChallengeFailed):
-    pass
+class CMagChallModelFailed(ChallFailed):
+    def __str__(self) -> str:
+        return f'CMagChallModelFailed'
 
-class CMagChallengeImplCreateError(CMagChallengeImplFailed): ...
-class CMagChallengeImplGetError(CMagChallengeImplFailed): ...
-class CMagChallengeImplSelectError(CMagChallengeImplFailed): ...
-class CMagChallengeImplDeleteError(CMagChallengeImplFailed): ...
+# CMagChallengeImpl class
+class CMagChallImplInitError(CMagChallImplFailed): 
+    def __str__(self) -> str:
+        return f'CMagChallImplInitError'
 
-class CMagChallengeManagerCreateError(CMagChallengeMangerFailed): ...
-class CMagChallengeManagerGetError(CMagChallengeMangerFailed): ...
-class CMagChallengeManagerSelectError(CMagChallengeMangerFailed): ...
-class CMagChallengeManagerDeleteError(CMagChallengeMangerFailed): ...
+class CMagChallImplRecordError(CMagChallImplFailed):
+    def __str__(self) -> str:
+        return f'CMagChallImplRecordError'
 
-class CMagChallengeModelError(CMagChallengeModelFailed): ...
+# CMagChallenge class
+class CMagChallInitError(CMagChallFailed): ...
+class CMagChallCreateError(CMagChallFailed): ...
+class CMagChallAddError(CMagChallFailed): ...
+class CMagChallGetError(CMagChallFailed): ...
+class CMagChallGetByPathError(CMagChallFailed): ...
+class CMagChallListError(CMagChallFailed): ...
+class CMagChallRemoveError(CMagChallFailed): ...
+
+# CMagChallengeManagerImpl class
+class CMagChallMgrImplInitError(CMagChallMgrImplFailed): ...
+class CMagChallMgrImplCreateError(CMagChallMgrImplFailed): 
+    def __str__(self) -> str:
+        return f'CMagChallMgrImplCreateError'
+class CMagChallMgrImplGetError(CMagChallMgrImplFailed): ...
+class CMagChallMgrImplGetByIdError(CMagChallMgrImplFailed): ...
+class CMagChallMgrImplSelectError(CMagChallMgrImplFailed): ...
+
+# CMagChallengeManager class
+class CMagChallMgrCreateError(CMagChallMgrFailed): ...
+class CMagChallMgrGetError(CMagChallMgrFailed): ...
+class CMagChallMgrSelectError(CMagChallMgrFailed): ...
+class CMagChallMgrDeleteError(CMagChallMgrFailed): ...
+
+# CMagChallengeModel class
+class CMagChallModelError(CMagChallModelFailed): ...
+
+# Exception raise decorator
+def Exception(exception):
+    def decorator(function):
+        def wrapper(self, *args, **kwargs):
+            try:
+                return function(self, *args, **kwargs)
+            except:
+                raise exception
+        return wrapper
+    return decorator
